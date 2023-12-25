f232002ce183ee962187556884f6db5432c658b2
peacemaker474 <hc456123@gmail.com>
2023-12-25 16:57:21 +0900

    feat: 데이터베이스 연결 기초 설정 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
-import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { Module } from '@nestjs/common';
 import { PostsModule } from './posts/posts.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [PostsModule],
+  imports: [
+    PostsModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: '127.0.0.1',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'postgres',
+      entities: [],
+      synchronize: true, // Production 환경에서는 false
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
