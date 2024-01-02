f9e9ec11438ab5a7aa9fed3db0e913483b733d32
peacemaker474 <hc456123@gmail.com>
2024-01-02 23:10:28 +0900

    feat: 유저 데코레이터 및 일부 가드 및 로직 수정

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -106,9 +106,13 @@ export class AuthService {
   }
 
   verifyToken(token: string) {
-    return this.jwtService.verify(token, {
-      secret: JWT_SECRET,
-    });
+    try {
+      return this.jwtService.verify(token, {
+        secret: JWT_SECRET,
+      });
+    } catch {
+      throw new UnauthorizedException('토큰이 만료됐거나, 잘못된 토큰입니다.');
+    }
   }
 
   reissueToken(token: string, isRefreshToken: boolean) {
---
+++ src/users/decorator/user.decorator.ts
@@ -0,0 +1,27 @@
+import {
+  ExecutionContext,
+  InternalServerErrorException,
+  createParamDecorator,
+} from '@nestjs/common';
+
+import { UsersModel } from '../entities/users.entity';
+
+export const User = createParamDecorator(
+  (data: keyof UsersModel | undefined, context: ExecutionContext) => {
+    const request = context.switchToHttp().getRequest();
+
+    const user = request.user as UsersModel;
+
+    if (!user) {
+      throw new InternalServerErrorException(
+        '요청 정보에 유저 정보가 존재하지 않습니다.',
+      );
+    }
+
+    if (data) {
+      return user[data];
+    }
+
+    return user;
+  },
+);
