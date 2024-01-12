498492c0f641e8b8fd58042170e90bcf396b3085
peacemaker474 <hc456123@gmail.com>
2024-01-12 21:55:08 +0900

    refactor: 이미지 업로드 컨트롤러 Common으로 분리

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -1,8 +1,48 @@
+import * as multer from 'multer';
+
+import { BadRequestException, Module } from '@nestjs/common';
+
+import { AuthModule } from 'src/auth/auth.module';
+import { CommonController } from './common.controller';
 import { CommonService } from './common.service';
-import { Module } from '@nestjs/common';
+import { MulterModule } from '@nestjs/platform-express';
+import { TEMP_FOLDER_PATH } from './constants/path';
+import { UsersModule } from 'src/users/users.module';
+import { extname } from 'path';
+import { v4 as uuid } from 'uuid';
 
 @Module({
+  imports: [
+    MulterModule.register({
+      limits: {
+        fileSize: 1000000,
+      },
+      fileFilter: (req, file, cb) => {
+        const ext = extname(file.originalname);
+
+        if (ext !== '.jpg' && ext !== '.jpeg' && ext !== '.png') {
+          return cb(
+            new BadRequestException('jpg/jpeg/png 파일만 업로드 가능합니다.'),
+            false,
+          );
+        }
+
+        return cb(null, true);
+      },
+      storage: multer.diskStorage({
+        destination: (req, res, cb) => {
+          cb(null, TEMP_FOLDER_PATH);
+        },
+        filename: (req, file, cb) => {
+          cb(null, `${uuid()}${extname(file.originalname)}`);
+        },
+      }),
+    }),
+    AuthModule,
+    UsersModule,
+  ],
   providers: [CommonService],
+  controllers: [CommonController],
   exports: [CommonService],
 })
 export class CommonModule {}
--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -1,18 +1,11 @@
-import * as multer from 'multer';
-
-import { BadRequestException, Module } from '@nestjs/common';
-
 import { AuthModule } from 'src/auth/auth.module';
 import { CommonModule } from 'src/common/common.module';
-import { MulterModule } from '@nestjs/platform-express';
-import { POST_IMAGE_PATH } from 'src/common/constants/path';
+import { Module } from '@nestjs/common';
 import { PostsController } from './posts.controller';
 import { PostsModel } from './entities/posts.entity';
 import { PostsService } from './posts.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from 'src/users/users.module';
-import { extname } from 'path';
-import { v4 as uuid } from 'uuid';
 
 @Module({
   imports: [
@@ -20,31 +13,6 @@ import { v4 as uuid } from 'uuid';
     AuthModule,
     UsersModule,
     CommonModule,
-    MulterModule.register({
-      limits: {
-        fileSize: 1000000,
-      },
-      fileFilter: (req, file, cb) => {
-        const ext = extname(file.originalname);
-
-        if (ext !== '.jpg' && ext !== '.jpeg' && ext !== '.png') {
-          return cb(
-            new BadRequestException('jpg/jpeg/png 파일만 업로드 가능합니다.'),
-            false,
-          );
-        }
-
-        return cb(null, true);
-      },
-      storage: multer.diskStorage({
-        destination: (req, res, cb) => {
-          cb(null, POST_IMAGE_PATH);
-        },
-        filename: (req, file, cb) => {
-          cb(null, `${uuid()}${extname(file.originalname)}`);
-        },
-      }),
-    }),
   ],
   controllers: [PostsController],
   providers: [PostsService],
