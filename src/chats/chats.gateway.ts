4aab2645667c3fb73e0f412ad559c6d175463185
peacemaker474 <hc456123@gmail.com>
2024-01-15 22:47:34 +0900

    feat: /chats 경로 broadcasting 추가

--- src/chats/chats.gateway.ts
+++ src/chats/chats.gateway.ts
@@ -1,22 +1,43 @@
 import {
+  ConnectedSocket,
   MessageBody,
   OnGatewayConnection,
   SubscribeMessage,
   WebSocketGateway,
+  WebSocketServer,
 } from '@nestjs/websockets';
 
-import { Socket } from 'socket.io';
+import { Server, Socket } from 'socket.io';
 
 @WebSocketGateway({
   namespace: 'chats',
 })
 export class ChatsGateway implements OnGatewayConnection {
+  @WebSocketServer()
+  server: Server;
+
   handleConnection(socket: Socket) {
     console.log(`on connect called: ${socket.id}`);
   }
 
+  @SubscribeMessage('enter_chat')
+  enterChat(@MessageBody() data: number[], @ConnectedSocket() socket: Socket) {
+    for (const chatId of data) {
+      socket.join(chatId.toString());
+    }
+  }
+
   @SubscribeMessage('send_message')
-  sendMessage(@MessageBody() data: string) {
-    console.log(data);
+  sendMessage(
+    @MessageBody()
+    message: {
+      message: string;
+      chatId: number;
+    },
+    @ConnectedSocket() socket: Socket,
+  ) {
+    socket
+      .to(message.chatId.toString())
+      .emit('receive_message', message.message);
   }
 }
